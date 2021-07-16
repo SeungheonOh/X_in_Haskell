eabc06026a91483134dbd2d160becef7747a0067
Seungheon Oh <dan.oh0721@gmail.com>
2021-07-15 22:31:30 -0500

    More greetings

--- helloworld/app/Main.hs
+++ helloworld/app/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
--- putStrLn simply writes String on the terminal!
+sayHello :: IO()
+sayHello = putStrLn "Hello from Function"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  putStrLn "Hello, Haskell!" -- putStrLn simply writes String on the terminal!
+  >> sayHello -- and of course we can use function to say hello
+  >> do -- Also, instead of using sequencing, do notaion is available for better readablility
+    putStrLn "Hello, Haskell! with do notation!"
+    sayHello
+  >> pure "Hello, Haskell! with binding!" -- let's tastes some binding operator here!
+  >>= putStrLn -- this putStrLn gets the value from previous sequence by binding. 
